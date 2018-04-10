f2063f7a36eb312382d3d1ef3faf4796bfec8811
Paulo  Diniz <paulo.rc.diniz@gmail.com>
2018-04-10 23:46:44 +0200

    create react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Pescotapa from './containers/Pescotapa';
+
+class App extends Component {
+  render() {
+    return (
+        <Pescotapa />
+    );
+  }
+}
+
+export default App;
---
+++ src/containers/Pescotapa.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+
+class Pescotapa extends Component {
+    render() {
+        return(<div id="pescotapa">
+               </div>)
+    }
+}
+
+export default Pescotapa;
