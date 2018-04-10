4b74ddec6374c0e7d61367d26c5ef92e82d0a809
Paulo  Diniz <paulo.rc.diniz@gmail.com>
2018-04-10 23:53:15 +0200

    activating ciro

--- src/containers/Pescotapa.js
+++ src/containers/Pescotapa.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 
-
 class Pescotapa extends Component {
+
+    constructor(props) {
+        super(props);
+        this.activateCiro = this.activateCiro.bind(this);
+    }
+
+    activateCiro() {
+        console.log("CIRO");
+    }
+
     render() {
-        return(<div id="pescotapa">
-               </div>)
+        return(
+            <div>
+              <div id="pescotapa">
+              </div>
+
+              <button onClick={this.activateCiro}>
+                ACTIVATE CIRO
+              </button>
+            </div>
+        )
     }
 }
 
