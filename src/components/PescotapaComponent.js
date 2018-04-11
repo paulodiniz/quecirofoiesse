5c4c1adc50643cf96ce84dc557545e9ed80d503c
Paulo  Diniz <paulo.rc.diniz@gmail.com>
2018-04-11 23:05:06 +0200

    started with tachyons for minor styling

--- src/components/PescotapaComponent.js
+++ src/components/PescotapaComponent.js
@@ -1,17 +1,19 @@
 import React from 'react';
 
-
 const PescotapaComponent = ({activateCiroFn, animated, animatedClassName, score}) => {
     return (
-        <div>
-          <div id="pescotapa" className={animatedClassName} />
+        <div className="dt mw6 center pt4">
+          <div class="db dtc-ns v-mid-ns">
+            <div id="pescotapa" className={animatedClassName} />
+          </div>
+          <div class="db dtc-ns v-mid ph2 pr0-ns pl3-ns">
+            <button onClick={activateCiroFn} disabled={animated}>
+              ACTIVATE CIRO
+            </button>
 
-          <button onClick={activateCiroFn} disabled={animated}>
-            ACTIVATE CIRO
-          </button>
-
-          <div>
-            <p> {score} </p>
+            <div>
+              <p> {score} </p>
+            </div>
           </div>
         </div>
     );
