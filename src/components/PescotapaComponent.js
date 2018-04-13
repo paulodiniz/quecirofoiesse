5b8ec7b5a8bae84ca079f26be0b6575780950d8a
Paulo  Diniz <paulo.rc.diniz@gmail.com>
2018-04-13 23:46:11 +0200

    Better styles

--- src/components/PescotapaComponent.js
+++ src/components/PescotapaComponent.js
@@ -2,17 +2,19 @@ import React from 'react';
 
 const PescotapaComponent = ({activateCiroFn, animated, animatedClassName, score}) => {
     return (
-        <div className="dt mw6 center pt4">
-          <div class="db dtc-ns v-mid-ns">
-            <div id="pescotapa" className={animatedClassName} />
-          </div>
-          <div class="db dtc-ns v-mid ph2 pr0-ns pl3-ns">
-            <button onClick={activateCiroFn} disabled={animated}>
-              ACTIVATE CIRO
-            </button>
-
-            <div>
-              <p> {score} </p>
+        <div>
+          <h1 className="f-subheadline lh-solid tc mb3 mt3" > Que Ciro Foi Esse? </h1>
+          <div className="dt mw9 center pt4">
+            <div className="db dtc-ns v-mid-ns">
+              <div id="pescotapa" className={animatedClassName} />
+            </div>
+            <div className="db dtc-ns v-mid ph2 pr0-ns pl4-ns">
+              <div className="f1 tc">
+                <p className="mb3"> {score} </p>
+              </div>
+              <button onClick={activateCiroFn} disabled={animated} className="bw0 br2 bg-blue pa2 white fw1 tc ttu tracked grow pointer">
+                Mamãe avisou
+              </button>
             </div>
           </div>
         </div>
